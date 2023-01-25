272a5204eb57c8766844c953487facf1badfb7fd
R Lopxhan <rlopxhan21@gmail.com>
2023-01-25 13:32:19 +1100

    Add final calc project

---
+++ script.js
@@ -0,0 +1,83 @@
+const display1Elm = document.querySelector(".display-1");
+const display2Elm = document.querySelector(".display-2");
+const tempResultElm = document.querySelector(".tempResult");
+
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const allClear = document.querySelector(".btn-ac");
+const clear = document.querySelector(".btn-c");
+
+let display1 = "";
+let display2 = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+numbers.forEach((num) => {
+  num.addEventListener("click", (event) => {
+    if (event.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (event.target.innerText === "." && haveDot) {
+      return;
+    }
+
+    display2 += event.target.innerText;
+    display2Elm.innerText = display2;
+  });
+});
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", (event) => {
+    if (!display2) {
+      return;
+    } else {
+      haveDot = false;
+      const operationName = event.target.innerText;
+
+      if (display1 && display2 && lastOperation) {
+        mathOperation();
+      } else {
+        result = parseFloat(display2);
+      }
+
+      clearVar(operationName);
+      lastOperation = operationName;
+    }
+  });
+});
+
+const clearVar = (optName = "") => {
+  display1 += display2 + " " + optName + " ";
+  display1Elm.innerText = display1;
+  display2Elm.innerText = "";
+  display2 = "";
+  tempResultElm.innerText = result;
+};
+
+const mathOperation = () => {
+  if (lastOperation === "X") {
+    result = parseFloat(result) * parseFloat(display2);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(display2);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(display2);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(display2);
+  } else if (lastOperation === "%") {
+    result = parseFloat(result) % parseFloat(display2);
+  }
+};
+
+equal.addEventListener("click", (event) => {
+  if (!display1 || !display2) {
+    return;
+  }
+
+  mathOperation();
+  clearVar();
+  display2Elm.innerText = result;
+  tempResultElm.innerText = "";
+  display1 = "";
+  display2 = result;
+});
